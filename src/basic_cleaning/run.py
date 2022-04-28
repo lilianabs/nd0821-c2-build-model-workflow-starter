2430e8ef3afcdf36b3796db9484767e7edd06822
lilianabs <lilianabsmath@gmail.com>
2022-04-28 16:05:10 -0500

    Drop outliers in the latitude and longitude column

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -38,6 +38,10 @@ def go(args):
     logger.info("Converting last_review to datetime") 
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    logger.info("Droppping outliers in the longitude and latitude columns")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("Saving artifact")
     filename = "clean_sample.csv"
     df.to_csv(filename, index=False)
